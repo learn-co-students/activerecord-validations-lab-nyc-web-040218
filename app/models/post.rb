e22edaf4e9323b5b4b703d1d278d2fb1f6e949b7
briecodes <hello@brie.work>
2018-04-30 09:23:58 -0400

    author validates defined. Post nearly complete.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates_with MyValidator
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: false}
+    validate :title
 end
