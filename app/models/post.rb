5bab20cbf24e4fe5208cfc4e3f5e99e1852aba2f
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-30 15:46:04 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
-  validate clickyBait?
+  validate :clickyBait?
 end
 
 def clickyBait?
