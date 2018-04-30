803a33b27d8c4b5ad0b381ed778d9ab7259bb978
Ahamed <ahamedali95@gmail.com>
2018-04-30 12:56:56 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,11 +13,8 @@ class Post < ActiveRecord::Base
   ]
 
   def is_clickbait
-    # if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-    #   errors.add(:title, "must be clickbait")
-    # end
-    if !CLICKBAIT_PATTERNS.include?(:title)
-       errors.add(:title, "must be clickbait")
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
     end
   end
 end
