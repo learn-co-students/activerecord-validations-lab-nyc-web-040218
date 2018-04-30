f21e9efc907032a9ad3c2e3e3e76284661cd433a
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-30 15:59:37 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,4 @@
-# class MyValidator < ActiveModel::Validator
-#   def validate(post)
-#     clicky_text = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-#     clicky_text.each do |ct|
-#       unless post.title.include?(ct)
-#         record.errors[:title] << 'Need a clickbait-y title please!'
-#       end
-#     end
-#   end
-# end
-
 class Post < ActiveRecord::Base
-  # include ActiveModel::Validations
-  # validates_with MyValidator
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
@@ -20,11 +7,15 @@ class Post < ActiveRecord::Base
   validate :clickyBait?
 end
 
+@@clicky_text = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
 def clickyBait?
-  clicky_text = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-  clicky_text.each do |ct|
-    unless self.title.include?(ct)
-      'Need a clickbait-y title please!'
-    end
+  if @@clicky_text.none? { |ct| ct.match title }
+    errors.add(:title, "Need a clickbait-y title please!")
   end
 end
